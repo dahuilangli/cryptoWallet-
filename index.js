c9718d17a21b2160a704ee81b6ea7fbe851b9c1c
jason <jason@xx.com>
2021-06-22 17:08:44 +0800

    i18n

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ import 'react-native-get-random-values';
 
 // 导入 ethers shims (**BEFORE** ethers)
 import '@ethersproject/shims';
+import 'utils/global';
 
 import { AppRegistry } from 'react-native';
 import App from 'routers/App';
 import { name as appName } from './app.json';
+
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/i18n/index.ts
@@ -0,0 +1,18 @@
+import i18next from 'i18next';
+import { initReactI18next, useTranslation} from 'react-i18next';
+
+import en from './en.json';
+import zh from './zh.json';
+
+i18next
+.use(initReactI18next)
+.init({
+    fallbackLng: 'en',
+    debug: true,
+    resources: {
+        en: {translation: en},
+        zh: {translation: zh},
+    }
+});
+
+export default i18next;
